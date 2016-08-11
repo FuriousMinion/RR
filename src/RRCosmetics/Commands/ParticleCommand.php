26a87766195ddcb2c568b43a685f7366f148a918
SpopovabgS <spopovabg@gmail.com>
2016-08-12 01:17:45 +0300

    Update ParticleCommand.php

--- src/RRCosmetics/Commands/ParticleCommand.php
+++ src/RRCosmetics/Commands/ParticleCommand.php
@@ -8,6 +8,8 @@ use pocketmine\plugin\Plugin;
 use pocketmine\Server;
 use pocketmine\Player;
 
+use pocketmine\utils\Config;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\CommandExecutor;
