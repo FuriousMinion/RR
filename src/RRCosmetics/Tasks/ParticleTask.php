6b4adc3d53152197327af3813dc6a05435bd9948
SpopovabgS <spopovabg@gmail.com>
2016-08-12 14:24:39 +0300

    Update ParticleTask.php

--- src/RRCosmetics/Tasks/ParticleTask.php
+++ src/RRCosmetics/Tasks/ParticleTask.php
@@ -19,29 +19,29 @@ use pocketmine\level\particle\BubbleParticle as bubble;
 use pocketmine\level\particle\CriticalParticle as critical;
 use pocketmine\level\particle\DustParticle as dust;
 use pocketmine\level\particle\EnchantParticle as enchant;
-use pocketmine\level\particle\InstantEnchantParticle as inenchant;
-use pocketmine\level\particle\ExplodeParticle as explode;
-use pocketmine\level\particle\LargeExplodeParticle as largeexplode;
-use pocketmine\level\particle\HugeExplodeParticle as hugeexplode;
+use pocketmine\level\particle\EnchantmentTableParticle as enchanttable;
 use pocketmine\level\particle\EntityFlameParticle as entflame;
+use pocketmine\level\particle\ExplodeParticle as explode;
 use pocketmine\level\particle\FlameParticle as flame;
+use pocketmine\level\particle\HappyVillagerParticle as happyvillager;
 use pocketmine\level\particle\HeartParticle as heart;
+use pocketmine\level\particle\HugeExplodeParticle as hugeexplode;
+use pocketmine\level\particle\InstantEnchantParticle as inenchant;
 use pocketmine\level\particle\InkParticle as ink;
 use pocketmine\level\particle\ItemBreakParticle as itembreak;
+use pocketmine\level\particle\LargeExplodeParticle as largeexplode;
 use pocketmine\level\particle\LavaDripParticle as lavadrip;
 use pocketmine\level\particle\LavaParticle as lava;
 use pocketmine\level\particle\PortalParticle as portal;
+use pocketmine\level\particle\RainSplashParticle as rainsplash;
 use pocketmine\level\particle\RedstoneParticle as redstone;
 use pocketmine\level\particle\SmokeParticle as smoke;
 use pocketmine\level\particle\SplashParticle as splash;
 use pocketmine\level\particle\SporeParticle as spore;
 use pocketmine\level\particle\TerrainParticle as terrain;
 use pocketmine\level\particle\WaterDripParticle as waterdrip;
-use pocketmine\level\particle\WaterParticle as water;
-use pocketmine\level\particle\EnchantmentTableParticle as enchanttable;
-use pocketmine\level\particle\HappyVillagerParticle as happyvillager;
 use pocketmine\level\particle\AngryVillagerParticle as angryvillager;
-use pocketmine\level\particle\RainSplashParticle as rainsplash;
+use pocketmine\level\particle\WaterParticle as water;
 use pocketmine\level\particle\DestroyBlockParticle as destroyblock;
 use pocketmine\level\particle\Particle;
 
@@ -64,9 +64,97 @@ class ParticleTask extends PluginTask {
       $pos = new Vector3($player->getX(), $player->getY() + 0.5, $player->getZ());
       $level = $this->plugin->getServer()->getLevelByName("Hub");
       switch($config->get($name)) {
+        case "bubble":
+          $level->addParticle(new bubble($pos));
+        break;
+        case "crit":
+        case "critical":
+          $level->addParticle(new critical($pos));
+        break;
+        case "ench":
+        case "enchant":
+          $level->addParticle(new enchant($pos));
+        break;
+        case "dust":
+          $level->addParticle(new dust($pos));
+        break;
+        case "inenchant":
+        case "instantenchant":
+          $level->addParticle(new inenchant($pos));
+        break;
+        case "explode":
+          $level->addParticle(new explode($pos));
+        break;
+        case "largeexplode":
+          $level->addParticle(new largeexplode($pos));
+        break;
+        case "hugeexplode":
+          $level->addParticle(new hugeexplode($pos));
+        break;
+        case "entflame":
+        case "entityflame":
+          $level->addParticle(new entflame($pos));
+        break;
         case "flame":
           $level->addParticle(new flame($pos));
         break;
+        case "heart":
+          $level->addParticle(new heart($pos));
+        break;
+        case "ink":
+          $level->addParticle(new ink($pos));
+        break;
+        case "lavadrip":
+          $level->addParticle(new lavadrip($pos));
+        break;
+        case "lava":
+          $level->addParticle(new lava($pos));
+        break;
+        case "portal":
+          $level->addParticle(new portal($pos));
+        break
+        case "portal":
+          $level->addParticle(new portal($pos));
+        break
+        case "redstone":
+          $level->addParticle(new redstone($pos));
+        break
+        case "splash":
+          $level->addParticle(new splash($pos));
+        break
+        case "spore":
+          $level->addParticle(new spore($pos));
+        break
+        case "terrain":
+          $level->addParticle(new terrain($pos));
+        break
+        case "waterdrip":
+          $level->addParticle(new waterdrip($pos));
+        break
+        case "water":
+          $level->addParticle(new water($pos));
+        break
+        case "enchtable":
+        case "enchanttable":
+          $level->addParticle(new enchanttable($pos));
+        break
+        case "happyvillager":
+          $level->addParticle(new happyvillager($pos));
+        break
+        case "angryvillager":
+          $level->addParticle(new angryvillager($pos));
+        break
+        case "rainsplash":
+          $level->addParticle(new ($pos));
+        break;
+        /*TO DO
+        case "itembreak":
+          $level->addParticle(new itembreak($pos));
+        break;
+        case "destroyblock":
+          $level->addParticle(new destroyblock($pos));
+        break;
+        */
       }
     }
   }
